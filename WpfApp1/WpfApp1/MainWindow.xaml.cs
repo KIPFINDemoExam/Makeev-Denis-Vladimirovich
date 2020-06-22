5b72a9c8d2290a378d2f75c27e7673134c84874f
unknown <KIPFINDemoExam>
2020-06-22 14:11:34 +0300

    first session

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,28 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    public class Product
+    {
+        public int id { get; set; }
+        public double cost { get; set; }
+        public string name { get; set; }
+        
+    }
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            SqlConnection connect = new SqlConnection("Data Source = DESKTOP-R0P0EBE\\MSSQLSRV;Uid = makeev;Password = xxXX1234;Database = Test");
+            SqlCommand com = new SqlCommand("SELECT Id,Cost,Description FROM Product", connect);
+            connect.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                var ss = new Product();
+                ss.id = Int32.Parse(reader.GetValue(0).ToString());
+                ss.cost = Int32.Parse(reader.GetValue(1).ToString());
+                ss.name = reader.GetValue(2).ToString();
+                lstBox.Items.Add(ss);
+
+            }
+            connect.Close();
         }
+       
+        
     }
 }
