17783e8d90edcf37b6c00a8354b0f50d7636cb5f
unknown <KIPFINDemoExam>
2020-06-22 15:53:09 +0300

    сессия2

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-
+using System.Collections;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
@@ -11,35 +11,63 @@ namespace WpfApp1
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public class Product
-    {
-        public int id { get; set; }
-        public double cost { get; set; }
-        public string name { get; set; }
-        
-    }
+    
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
+
             InitializeComponent();
             SqlConnection connect = new SqlConnection("Data Source = DESKTOP-R0P0EBE\\MSSQLSRV;Uid = makeev;Password = xxXX1234;Database = Test");
-            SqlCommand com = new SqlCommand("SELECT Id,Cost,Description FROM Product", connect);
             connect.Open();
+            SqlCommand com = new SqlCommand("SELECT Cost,Description FROM Product", connect);
             SqlDataReader reader = com.ExecuteReader();
-            while (reader.Read())
+            if (reader.HasRows)
             {
-                var ss = new Product();
-                ss.id = Int32.Parse(reader.GetValue(0).ToString());
-                ss.cost = Int32.Parse(reader.GetValue(1).ToString());
-                ss.name = reader.GetValue(2).ToString();
-                lstBox.Items.Add(ss);
-
+                while (reader.Read())
+                {
+                    UserControl1 product = new UserControl1(reader.GetValue(2).ToString(), Int32.Parse(reader.GetValue(1).ToString()));
+                    lstBox.Items.Add(product);
+                }
+                connect.Close();
             }
-            connect.Close();
+            else
+            {
+                MessageBox.Show("Ничего в таблице нету");
+                connect.Close();
+            }
         }
-       
-        
+
+        private void Search_Product(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (search.Text == "Введите имя товара которое для поиска")
+            {
+                search.Text = "";
+            }
+            else
+            {
+                SqlConnection connect = new SqlConnection("Data Source = DESKTOP-R0P0EBE\\MSSQLSRV;Uid = makeev;Password = xxXX1234;Database = Test");
+                connect.Open();
+                string searchProduct = search.Text;
+                SqlCommand com = new SqlCommand("SELECT Cost,Description FROM Product WHERE Description = 'searchProduct'", connect);
+                SqlDataReader reader = com.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    UserControl1 product = new UserControl1(reader.GetValue(2).ToString(), Int32.Parse(reader.GetValue(1).ToString()));
+                    lstBox.Items.Add(product);
+                    connect.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Данного товара нету в наличии");
+                    connect.Close();
+                }
+            }
+        }
+
+        private void Add_Product(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
